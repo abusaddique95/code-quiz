4dadd62093e16c303ac666ca58837e86d8acb479
abusaddique95 <abusaddique95@live.com>
2022-04-28 17:54:02 +0100

    set up folders and moved pseduo code over

---
+++ assets/js/index.js
@@ -0,0 +1,92 @@
+// global declarations
+const questions = [];
+let questionIndex = 0;
+let timerValue = 10 * questions.length;
+let quizComplete = false;
+
+const onLoad = () => {
+  // initialise local storage
+  // check if highscores exists in LS
+  // if false then set highscores to empty array in LS
+};
+
+const removeStartSection = () => {};
+
+const startTimer = () => {
+  // declare function to execute every 1 sec
+  const countdown = () => {
+    // decrement timer value
+    // if quizComplete is true then stop timer
+    // check if timer reaches 0
+    // if true render game over
+  };
+
+  // setInterval of 1000ms (1s)
+};
+
+const validateAnswer = () => {
+  // get answer clicked from user
+  // get the correct answer for question
+  // compare the 2 answers
+  // if incorrect subtract 5 seconds from timerValue
+  // if incorrect render error alert with message and status
+  // if correct render success alert with message and status
+  // set timeout for 500ms and then go to next question
+  // if question is last question set quizComplete to true and then render form
+  // if question is not last question then increment question index and render next question
+};
+
+const handleFormSubmit = () => {
+  // get value from input
+  // check if empty then render error alert with message and status
+  // if not empty then create the score object
+  // {
+  //   fullName: "Bob Smith",
+  //   score: 25
+  // }
+  // push score object to LS
+  // render quizCompleteSection
+};
+
+const renderTimerSection = () => {
+  // use HTML as guide and build in JS
+  // append section to main
+};
+
+const renderQuestionSection = () => {
+  // use HTML as guide and build in JS
+  // append section to main
+  // add click event listener on #question-section
+};
+
+const renderGameOver = () => {
+  // use HTML as guide and build in JS
+  // append section to main
+};
+
+const renderAlert = (message, status) => {
+  // use HTML as guide and build in JS
+  // append div to #question-section
+};
+
+const renderForm = () => {
+  // use HTML as guide and build in JS
+  // append section to main
+  // add submit event handler to form
+};
+
+const renderQuizCompleteSection = () => {
+  // use HTML as guide and build in JS
+  // append section to main
+};
+
+const startQuiz = () => {
+  // remove start section
+  // start timer
+  // render timer section
+  // render question section
+};
+
+// add event listeners
+// add document on load event listener
+// add start button click event listener
