4feb68fbd9eabbb4506829b9d864c568218a5de3
abusaddique95 <abusaddique95@live.com>
2022-05-02 02:56:44 +0100

    added html framework

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,40 @@
+const startButton = document.getElementById("start-quiz-btn");
+const mainSection = document.getElementById("main");
+const questionSection = document.getElementById("question-section");
+const timerSpan = document.getElementById("timer-span");
+const formSection = document.getElementById("form-section");
+const fullNameInput = document.getElementById("full-name-input");
+
 // global declarations
-const questions = [];
+const questions = [
+  {
+    question: "Whats the answer to number 1?",
+    choices: ["One", "Two", "Three", "Four"],
+    answer: "One",
+  },
+  {
+    question: "Whats the answer to number 2?",
+    choices: ["One", "Two", "Three", "Four"],
+    answer: "Two",
+  },
+  {
+    question: "Whats the answer to number 3?",
+    choices: ["One", "Two", "Three", "Four"],
+    answer: "Three",
+  },
+  {
+    question: "Whats the answer to number 4?",
+    choices: ["One", "Two", "Three", "Four"],
+    answer: "Four",
+  },
+];
+
 let questionIndex = 0;
 let timerValue = 10 * questions.length;
 let quizComplete = false;
 
 const onLoad = () => {
+  console.log("hello");
   // initialise local storage
   // check if highscores exists in LS
   // if false then set highscores to empty array in LS
@@ -34,6 +64,8 @@ const validateAnswer = () => {
   // set timeout for 500ms and then go to next question
   // if question is last question set quizComplete to true and then render form
   // if question is not last question then increment question index and render next question
+  questionIndex++;
+  renderQuestionSection();
 };
 
 const handleFormSubmit = () => {
@@ -55,8 +87,28 @@ const renderTimerSection = () => {
 
 const renderQuestionSection = () => {
   // use HTML as guide and build in JS
-  // append section to main
+  //Create Quesiton Section
+  const questionSection = document.createElement("section");
+  //Create h3 element and give text content of current question
+  const question = document.createElement("h3");
+  question.textContent = questions[questionIndex].question;
+  //Append question h3 element to the question section
+  questionSection.appendChild(question);
+  //Loop over question choices and create buttons for each and append to question section
+  const currentChoices = questions[questionIndex].choices;
+  console.log(questionIndex, currentChoices);
+  for (let i = 0; i < currentChoices.length; i++) {
+    const button = document.createElement("button");
+    button.textContent = currentChoices[i];
+    button.onclick = validateAnswer;
+    questionSection.appendChild(button);
+  }
+  console.log("question section", questionSection);
+
+  // append question section to main
+  mainSection.appendChild(questionSection);
   // add click event listener on #question-section
+  questionSection.addEventListener("click");
 };
 
 const renderGameOver = () => {
@@ -81,12 +133,17 @@ const renderQuizCompleteSection = () => {
 };
 
 const startQuiz = () => {
+  console.log("start quiz button click");
   // remove start section
-  // start timer
+  startButton.setAttribute("class", "hide");
+  // start timer (tuesday session)
   // render timer section
   // render question section
+  renderQuestionSection();
 };
 
 // add event listeners
 // add document on load event listener
+
 // add start button click event listener
+startButton.addEventListener("click", startQuiz);
